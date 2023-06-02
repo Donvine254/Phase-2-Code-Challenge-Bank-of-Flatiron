a9ecf1bf26c3feeec873449df7e2c72566ff6957
donvine mugendi <donvinemugendi@gmail.com>
2023-06-02 23:18:52 +0300

    added function to add transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,7 +3,7 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
-const baseUrl = "http://localhost:8001/transactions";
+export const baseUrl = "http://localhost:8001/transactions";
 
 function AccountContainer() {
   const [transactions, setTransactions] = useState([]);
@@ -25,16 +25,21 @@ function AccountContainer() {
     }
 
     return () => {};
-  }, []);
+  }, [transactions]);
 //handle searching transactions
   const transactionsToDisplay = transactions.filter((transaction) =>
     transaction.description.toLowerCase().includes(search.toLowerCase())
   );
 
+  function handleNewTransactions(newTransaction){
+    // setTransactions((prevTD)=>[...prevTD, newTransaction])
+    // console.log(newTransaction)
+  }
+
   return (
     <div>
       <Search search={search} onSearchChange={setSearch} />
-      <AddTransactionForm />
+      <AddTransactionForm onFormSubmit={handleNewTransactions} />
       <TransactionsList transactions={transactionsToDisplay} />
     </div>
   );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { baseUrl } from "./AccountContainer";
 
-function AddTransactionForm() {
+function AddTransactionForm({ onFormSubmit }) {
+  //initialize form data as an a object to avoid having repetitive onChange functions
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: 0,
+  });
+  //single function to handle change
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+  //function to handleFormSubmission
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const isEmpty = Object.values(formData).some((value) => value === ""); //checking for empty values
+    if (isEmpty) {
+      Swal.fire({
+        title: "An error occurred",
+        icon: "error",
+        text: "Please fill out all fields before submitting!",
+        showCloseButton: true,
+        timer: 3000,
+      });
+      return;
+    }
+    Swal.fire({
+      icon: "success",
+      text: "Transaction added successfully!",
+      title: "success",
+      showCloseButton: true,
+      timer: 3000,
+    });
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((newTransaction) => onFormSubmit(newTransaction));
+    event.target.value = "";
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            value={formData.category}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            step="0.01"
+            value={formData.amount}
+            onChange={handleChange}
+          />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
